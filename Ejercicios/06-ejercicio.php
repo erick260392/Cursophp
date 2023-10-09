ac196b677fd7b931b10d6ffffa45321fc12ffea5
erick260392 <erickliconamorales2016@gmail.com>
2023-10-09 12:50:42 -0600

    ejercicio6

--- Ejercicios/06-ejercicio.php
+++ Ejercicios/06-ejercicio.php
@@ -18,4 +18,18 @@ for ($cabecera=1; $cabecera <=10 ; $cabecera++) {
 
 echo "</tr>"; //cierre fila 1 de celdas
 
+echo "<tr>"; //inicio fila 2
+
+for ($i=1; $i <=10 ; $i++) { 
+    echo "<td>";
+
+    for ($x=1; $x <=10 ; $x++) { 
+        echo "$i x $x = " .($i * $x) . "<br/>";
+    }
+
+    echo "</td>";
+}
+
+echo "</tr>"; //cierre fila 2
+
 echo "</table"; //fin de la tabla
