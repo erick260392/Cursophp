94718ab5a4e6628dada71a8eef9f1b97798a6a1c
erick260392 <erickliconamorales2016@gmail.com>
2023-10-09 11:47:32 -0600

    Mensaje de commit

--- Ejercicios/06-ejercicio.php
+++ Ejercicios/06-ejercicio.php
@@ -6,3 +6,16 @@
 // mostrar todas las tablas de multiplicar del 1 al 10 en una tabla
 
 
+echo "<table border=1> <tr>"; //inicio de la tabla
+
+echo "<tr>"; //fila 1 de celdas
+
+for ($cabecera=1; $cabecera <=10 ; $cabecera++) { 
+
+    echo "<td>Tabla del $cabecera</td>";
+ 
+}
+
+echo "</tr>"; //cierre fila 1 de celdas
+
+echo "</table"; //fin de la tabla
